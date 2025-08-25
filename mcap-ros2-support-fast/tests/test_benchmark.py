f4169902182980cdc5ddea008c14b258fa4430d3
Marko Bausch <60338487+mrkbac@users.noreply.github.com>
2025-08-25 08:28:24 +0200

    Added benchmark results to readme

--- mcap-ros2-support-fast/tests/test_benchmark.py
+++ mcap-ros2-support-fast/tests/test_benchmark.py
@@ -8,7 +8,7 @@ from mcap_ros2_support_fast._dynamic import create_decoder
 from mcap_ros2_support_fast.decoder import DecoderFactory as DecoderFactoryFast
 
 
-def _read_all(factory):
+def _read_all(factory, msgs: int):
     file = (
         Path(__file__).parent.parent.parent
         / "data"
@@ -18,16 +18,22 @@ def _read_all(factory):
 
     with file.open("rb") as f:
         reader = make_reader(f, decoder_factories=[factory])
-        for _ in itertools.islice(reader.iter_decoded_messages(), 100):
+        for _ in itertools.islice(reader.iter_decoded_messages(), msgs):
             pass
 
 
 @pytest.mark.parametrize(
-    "factory",
+    ("factory", "msgs"),
     [
-        pytest.param(DecoderFactory(), id="mcap_ros2"),
-        pytest.param(DecoderFactoryFast(create_decoder), id="mcap_ros2_fast"),
+        pytest.param(factory, msgs, id=f"{name}-{msgs}")
+        for factory, name in [
+            (DecoderFactory(), "mcap_ros2"),
+            (DecoderFactoryFast(create_decoder), "mcap_ros2_fast"),
+        ]
+        for msgs in [10, 100, 1_000]
     ],
 )
-def test_benchmark_decoder(benchmark, factory):
-    benchmark(_read_all, factory)
+@pytest.mark.benchmark(group="msgs-")
+def test_benchmark_decoder(benchmark, factory, msgs):
+    benchmark.group += str(msgs)
+    benchmark(_read_all, factory, msgs)
