53e1a9b8e5b3b2bcae1c7daa66ad4aad8d3f96b4
Marko Bausch <60338487+mrkbac@users.noreply.github.com>
2025-11-18 08:48:39 +0100

    fix: Update _get_field to use Mapping for type checking

--- mcap-ros2-support-fast/scripts/generate_code.py
+++ mcap-ros2-support-fast/scripts/generate_code.py
@@ -93,7 +93,7 @@ def main() -> None:
             "# Utility functions",
             f"{UTF8_FUNC_NAME} = codecs.utf_8_decode",
             "_encode_utf8 = codecs.utf_8_encode",
-            "_get_field = lambda obj, *field_path: (__import__('functools').reduce(lambda o, f: o[f] if isinstance(o, dict) else getattr(o, f), field_path, obj))",  # noqa: E501
+            "_get_field = lambda obj, f: (obj[f] if isinstance(obj, (__import__('collections.abc').Mapping)) else getattr(obj, f))",
             "",
         ]
     )
